a2c2892c841940e4098bf1d686d399df7187f4b6
Silenthinker <873090016@qq.com>
2017-10-07 02:16:29 +0800

    uptodate

--- generate.py
+++ generate.py
@@ -54,6 +54,7 @@ def main():
                                    stop_early=(not args.no_early_stop),
                                    normalize_scores=(not args.unnormalized),
                                    len_penalty=args.lenpen)
+                                   ###enable_topic=args.enable_topic)
     align_dict = {}
     if args.unk_replace_dict != '':
         assert args.interactive, "Unkown words replacing requires access to original source and is only" \
