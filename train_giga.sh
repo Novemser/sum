fbb7f03a5bb18988794cd432b720f330cc40c79f
Silenthinker <sparklethinker@gmail.com>
2017-10-09 03:07:42 +0800

    update training bash

--- train_giga.sh
+++ train_giga.sh
@@ -1,3 +1,3 @@
 mkdir -p checkpoints/fconv
 ##CUDA_VISIBLE_DEVICES=1 python3 train.py data_giga_bin --lr 0.25 --clip-norm 0.1 --dropout 0.2 --max-tokens 4000 --arch fconv_giga --save-dir checkpoints/fconv
-CUDA_VISIBLE_DEVICES=1 python3 train.py data_giga_bin --lr 0.25 --clip-norm 0.1 --dropout 0.1 --max-tokens 4000 --arch fconv_giga --save-dir checkpoints/fconv
+python3 train.py data_giga_bin --lr 0.5 --clip-norm 0.1 --dropout 0.15 --max-tokens 4200  --max-len-b 50 --arch fconv_giga --save-dir checkpoints/fconv --cuda-visible-devices 1 --workers 2 --save-interval 3000
